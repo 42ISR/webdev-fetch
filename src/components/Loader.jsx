28c5a7f2478ba926eb904b14591f817ff946acdc
Kutikov Pasha <kutikovpasha@gmail.com>
2026-09-12 11:46:22 +0600

    add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="loader-content">
+                <div className="loader-spinner"></div>
+                <p className="loader-text">Загружаем книги...</p>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
